d6e46f71a078c0b3f20a4fc9fd6a97974392c528
Bruno Pinheiro <bruno@artnew.com.br>
2020-10-18 18:14:49 -0300

    logic fixes

--- lifeCounter/App.js
+++ lifeCounter/App.js
@@ -13,30 +13,44 @@ import {Container,
 
 export const App = () => {
   let currentDate = new Date();
-  const [birthdate, setBirthdate]       = useState(new Date());
   const [yearsLived, setYearsLived]     = useState(0);
   const [monthsLived, setMonthsLived]   = useState(0);
   const [daysLived, setDaysLived]       = useState(0);
   const [showLifeTime, setShowLifeTime] = useState(true);
+  const [monthsText, setMonthsText]     = useState('meses');
 
+  /**
+   * Calculate how many years, months and days a given person has lived
+   * @param {*} event 
+   * @param {*} selectedDate 
+   */
   function calculateLifetime(event, selectedDate) {
-    let tempYears  = 0;
-    let tempMonths = 0;
-    let tempDays   = 0;
-
     console.log('----------------------------------------');
     selectedDate = new Date('1989-09-02T03:00:00');
     currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
 
-    // Calculate years
+    calculateYears(selectedDate);
+    calculateMonths(selectedDate);
+    calculateDays(selectedDate);
+
+    setShowLifeTime(true);
+  }
+
+  
+  function calculateYears(selectedDate) {
+    let tempYears = 0;
+
     tempYears = currentDate.getFullYear() - selectedDate.getFullYear()
     if (currentDate.getMonth() < selectedDate.getMonth()) {
       tempYears--;
     }
     console.log(`tempYears`, tempYears);
     setYearsLived(tempYears);
+  }
 
-    // Calculate months
+  function calculateMonths(selectedDate) {
+    let tempMonths = 0;
+
     if (currentDate.getMonth() < selectedDate.getMonth()) {
       tempMonths = (currentDate.getMonth()+1) + (12 - (selectedDate.getMonth()+1));
     } else if (currentDate.getMonth() > selectedDate.getMonth()) {
@@ -44,15 +58,49 @@ export const App = () => {
     } else {
       tempMonths = 0;
     }
+
+    if (currentDate.getDate() < selectedDate.getDate()) {
+      tempMonths--;
+    }
+
     console.log(`tempMonths`, tempMonths);
     setMonthsLived(tempMonths);
 
-    // Calculate days
-    tempDays = currentDate.getDate();
+    if (tempMonths == 1) {
+      setMonthsText('mês');
+    } else {
+      setMonthsText('meses');
+    }
+  }
+
+  function calculateDays(selectedDate) {
+    let tempDays = 0;
+
+    if (currentDate.getDate() > selectedDate.getDate()) {
+      tempDays = currentDate.getDate() - selectedDate.getDate();
+    } else {
+      tempDays = getRemainingDays(selectedDate) + currentDate.getDate();
+    }
     console.log(`tempDays`, tempDays);
     setDaysLived(tempDays);
+  }
 
-    setShowLifeTime(true);
+  /**
+   * Get the days left until the end of month
+   * @param {*} dateInTest 
+   */
+  function getRemainingDays(dateInTest) {
+    let date;
+    let time;
+    let days;
+
+    date = new Date(dateInTest.getFullYear(), dateInTest.getMonth(), dateInTest.getDate()); 
+    time = new Date(date.getTime());
+    time.setMonth(date.getMonth() + 1);
+    time.setDate(0);
+    days = time.getDate() > date.getDate() ? time.getDate() - date.getDate() : 0;
+
+    return days;
   }
 
   return (
@@ -62,32 +110,32 @@ export const App = () => {
           <BirthdateLabel>Data de nascimento</BirthdateLabel>
           <DatePickerOuter>
             <DatePicker
-            date={birthdate}
-            androidMode="spinner"
-            format="DD-MM-YYYY"
-            minDate="01-01-1900"
-            onDateChange={(event, date) => calculateLifetime(event, date)}
-            customStyles={{
-              dateTouchBody: {
-                fontSize: 26,
-                width: 200,
-              },
-              dateIcon: {
-                position: 'absolute',
-                left: 0,
-                top: 4,
-                marginLeft: 0
-              },
-              dateInput: {
-                marginLeft: 36,
-                borderColor: '#2d6187',
-                // borderWidth: 0,
-              },
-              dateText: {
-                color: '#2d6187',
-                fontSize: 24,
-              }
-            }}
+              date={new Date()}
+              androidMode="spinner"
+              format="DD-MM-YYYY"
+              minDate="01-01-1900"
+              onDateChange={(event, date) => calculateLifetime(event, date)}
+              customStyles={{
+                dateTouchBody: {
+                  fontSize: 26,
+                  width: 200,
+                },
+                dateIcon: {
+                  position: 'absolute',
+                  left: 0,
+                  top: 4,
+                  marginLeft: 0
+                },
+                dateInput: {
+                  marginLeft: 36,
+                  borderColor: '#2d6187',
+                  // borderWidth: 0,
+                },
+                dateText: {
+                  color: '#2d6187',
+                  fontSize: 24,
+                }
+              }}
             />
           </DatePickerOuter>
         </BirthdateSelection>
@@ -98,7 +146,7 @@ export const App = () => {
           <LifetimeOuter>
             <YourAgeLabel>Você tem</YourAgeLabel>
             <Lifetime>{yearsLived} anos</Lifetime>
-            <Lifetime>{monthsLived} meses</Lifetime>
+            <Lifetime>{monthsLived} {monthsText}</Lifetime>
             <Lifetime>{daysLived} dias</Lifetime>
           </LifetimeOuter>
 
